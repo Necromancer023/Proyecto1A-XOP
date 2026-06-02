27d76167ee9a3cdb23614b202fcbf351448bf740
Mario Barboza <mbarbozaartavia17@gmail.com>
2026-06-02 15:40:20 -0600

    Se agrega Para el flujo del juego

---
+++ Game_Flow.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef GAME_FLOW_H
+#define GAME_FLOW_H
+
+// ================================
+// FUNCIONES DE FLUJO DEL JUEGO
+// Declaradas en main.cpp,
+// usadas desde screens.cpp y player.cpp
+// para evitar dependencias 
+// ================================
+void start_new_game();
+
+#endif
